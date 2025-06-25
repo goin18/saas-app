6a71581148feea40f243d78f7e87f2d165ee65d4
Marko Budal <markobudal@gmail.com>
2025-06-25 12:19:51 +0200

    subscription check

--- lib/actions/companion.actions.ts
+++ lib/actions/companion.actions.ts
@@ -109,6 +109,40 @@ export const getUserCompanions = async (userId: string) => {
     return data;
 }
 
+export const newCompanionPermissions = async () => {
+    const { userId, has } = await auth();
+    const supabase = createSupabaseClient();
+
+    let limit = 0;
+
+    // console.log(`has({ plan: 'pro' }): ${has({ plan: 'pro' })}`);
+    // console.log(`has({ feature: "3_companion_limit" }): ${has({ feature: "3_companion_limit" })}`);
+    // console.log(`has({ feature: "10_companion_limit" }): ${has({ feature: "10_companion_limit" })}`);
+
+    if(has({ plan: 'pro' })) {
+        return true;
+    } else if(has({ feature: "3_companion_limit" })) {
+        limit = 3;
+    } else if(has({ feature: "10_companion_limit" })) {
+        limit = 10;
+    }
+
+    const { data, error } = await supabase
+        .from('companions')
+        .select('id', { count: 'exact' })
+        .eq('author', userId)
+
+    if(error) throw new Error(error.message);
+
+    const companionCount = data?.length;
+
+    if(companionCount >= limit) {
+        return false
+    } else {
+        return true;
+    }
+}
+
 // Bookmarks
 export const addBookmark = async (companionId: string, path: string) => {
     const { userId } = await auth();
